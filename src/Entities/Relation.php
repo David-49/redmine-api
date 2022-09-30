a1bcc1fa2d6739a407cb239ab9e06b33a5c52f26
David-49 <daviddognin@MacBook-Pro-de-David.local>
2022-09-30 14:47:30 +0200

    Utilisation de MapFrom pour mapper le nom de base de la source avec un nouveau en camelCase

--- src/Entities/Relation.php
+++ src/Entities/Relation.php
@@ -2,13 +2,21 @@
 
 namespace OuestCode\RedmineApi\Entities;
 
+use Spatie\DataTransferObject\Attributes\MapFrom;
 use Spatie\DataTransferObject\DataTransferObject;
 
 class Relation extends DataTransferObject
 {
     public int $id;
+
+    #[MapFrom('issue_id')]
     public int $issueId;
+
+    #[MapFrom('issue_to_id')]
     public int $issueToId;
+
+    #[MapFrom('relation_type')]
     public string $relationType;
+
     public ?string $delay;
 }
